9f14be55aadb7254d7bb1e0cc0a7dae20a601c42
FryeT <fengleitan@hotmail.com>
2016-09-07 19:00:21 +0800

    add examples

---
+++ examples/socket_preview.py
@@ -0,0 +1,33 @@
+"""
+sockets for cross-task communication: start threads to communicate over sockets; independent programs can too, beacause sockets are system-wide, much like fifos; see the GUI and Internet parts of the book for more realistic socket use case; some socket servers may also need to talk to clients in threads or processes; sockets pass byte strings, but can be pickled objects or encoded Unicode text; caveat: print in threads may need to be synchronized if their output overlaps;
+"""
+from socket import socket, AF_INET, SOCK_STREAM
+
+port = 50008
+host = 'localhost'
+
+def server():
+    sock = socket(AF_INET, SOCK_STREAM) # ip address tcp connection
+    sock.bind(('', port))               # bind to port on this machine
+    sock.listen(50)                      # allow up to 5 pending client
+    while True:
+        conn, addr = sock.accept()      # wait for client to connect
+        data = conn.recv(1024)          # read bytes data from this client
+        reply = 'server got: [%s]' % data      # conn is a new connected socket
+        conn.send(reply.encode())
+
+def client(name):
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.connect((host, port))
+    sock.send(name.encode())
+    reply = sock.recv(1024)
+    sock.close()
+    print('client got: [%s]' % reply)
+
+if __name__ == '__main__':
+    from threading import Thread
+    sthread = Thread(target=server)
+    sthread.daemon = True
+    sthread.start()
+    for i in range(50):
+        Thread(target=client, args=('client%s' % i,)).start()
---
+++ examples/try.py
@@ -0,0 +1,3 @@
+import sys
+stdinfd = sys.stdin.fileno()
+print(stdinfd)
